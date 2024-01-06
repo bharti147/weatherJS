26a9805187971a7dbe6fa208fe8a7115ea3fbaf9
Bharti <sahanibharti71@gmail.com>
2024-01-06 20:43:27 +0530

    checking whether the api is working or not

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 document.addEventListener("DOMContentLoaded",()=>{
     const cityInput = document.getElementById("cityInput");
-    const apiKey="3fa1061ad94f010fc7b8a82562e6b32c";
+    const apiKey="ddb60c60493386bfe579d47f9d3bf075";
     const getWeatherBtn = document.getElementById("getWeatherBtn");
     const weatherInfo = document.getElementById("weatherInfo");
     const weatherCity= weatherInfo.querySelector('h2');
     const weatherDescription=weatherInfo.querySelector('p');
 
+   
     getWeatherBtn.addEventListener("click",()=>{
-        const city= cityInput.ariaValueMax.trim();
+        const city= cityInput.value.trim();
         if(city!==""){
             getWeather(city);
             cityInput.value="";
         }
     })
+    async function getWeather(city){
+        const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+  try{
+      const response=await fetch(url);
+      if(!response.ok){
+          throw new Error(`Network resonse was not ok: ${response.statusText}`)
+      }
+      const data=await response.json();
+      showWeather(data);
+      console.log("data from the api: ",data);
+  }
+  catch(error){
+  console.log('Errors fetching data:',error);
+  }
+      }
 
-    getWeather(city){
-      const url= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+      function showWeather(data) {
+        const cityName = data.name;
+        const weatherDescription = data.weather[0].description;
+        const temperature = data.main.temp;
+    
+        weatherCity.textContent = cityName;
+        weatherInfo.style.display = "block";
+        weatherDescription.textContent = `Weather: ${weatherDescription}, Temperature: ${temperature}°C`;
+      }
 
-    }
+   
+  
 })
